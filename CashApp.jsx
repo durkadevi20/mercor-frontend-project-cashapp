e0774262633fc4de7bd533ee922e8ef30e21a233
durkadevi20 <100428564+durkadevi20@users.noreply.github.com>
2023-08-22 20:38:38 +0530

    CashApp.jsx

---
+++ CashApp.jsx
@@ -0,0 +1,116 @@
+import React from "react";
+import "./style.css";
+
+export const CashApp = () => {
+  return (
+    <div className="cash-app">
+      <div className="hero-section-wrapper">
+        <div className="hero-section">
+          <div className="overlap">
+            <img
+              className="eye-button"
+              alt="Eye button"
+              src="https://generation-sessions.s3.amazonaws.com/269a3452e1bde0a42651d0f732bc5a39/img/eye-button.svg"
+            />
+            <div className="navbar">
+              <div className="text-wrapper">SIGN IN</div>
+              <div className="text-wrapper">LEGAL</div>
+              <div className="licenses">LICENSES</div>
+              <div className="text-wrapper">SECURITY</div>
+              <div className="text-wrapper">CAREERS</div>
+              <div className="text-wrapper">PRESS</div>
+              <div className="text-wrapper">SUPPORT</div>
+              <div className="text-wrapper">STATUS</div>
+              <div className="text-wrapper">CODEBLOG</div>
+            </div>
+            <img
+              className="cashapp-logo"
+              alt="Cashapp logo"
+              src="https://generation-sessions.s3.amazonaws.com/269a3452e1bde0a42651d0f732bc5a39/img/cashapp---logo.svg"
+            />
+            <div className="rays">
+              <div className="hero">
+                <div className="overlap-group">
+                  <div className="div">
+                    <div className="overlap-2">
+                      <div className="text-wrapper-2">CASH</div>
+                      <img
+                        className="intro-phone"
+                        alt="Intro phone"
+                        src="https://generation-sessions.s3.amazonaws.com/269a3452e1bde0a42651d0f732bc5a39/img/intro-phone-1@2x.png"
+                      />
+                      <div className="APP">
+                        <br />
+                        APP
+                      </div>
+                      <img
+                        className="intro-cubes"
+                        alt="Intro cubes"
+                        src="https://generation-sessions.s3.amazonaws.com/269a3452e1bde0a42651d0f732bc5a39/img/intro-cubes-1@2x.png"
+                      />
+                      <img
+                        className="intro-pillar"
+                        alt="Intro pillar"
+                        src="https://generation-sessions.s3.amazonaws.com/269a3452e1bde0a42651d0f732bc5a39/img/intro-pillar-1@2x.png"
+                      />
+                    </div>
+                    <img
+                      className="intro-stairs"
+                      alt="Intro stairs"
+                      src="https://generation-sessions.s3.amazonaws.com/269a3452e1bde0a42651d0f732bc5a39/img/intro-stairs-1@2x.png"
+                    />
+                  </div>
+                  <img
+                    className="intro-cube"
+                    alt="Intro cube"
+                    src="https://generation-sessions.s3.amazonaws.com/269a3452e1bde0a42651d0f732bc5a39/img/intro-cube-1@2x.png"
+                  />
+                </div>
+              </div>
+            </div>
+            <div className="fixed-footer">
+              <div className="download-icons">
+                <div className="buttons">
+                  <div className="app-store">
+                    <img
+                      className="vector"
+                      alt="Vector"
+                      src="https://generation-sessions.s3.amazonaws.com/269a3452e1bde0a42651d0f732bc5a39/img/vector-2.svg"
+                    />
+                    <div className="label">APP STORE</div>
+                  </div>
+                  <div className="google-play">
+                    <img
+                      className="img"
+                      alt="Vector"
+                      src="https://generation-sessions.s3.amazonaws.com/269a3452e1bde0a42651d0f732bc5a39/img/vector-1.svg"
+                    />
+                    <div className="label-2">GOOGLE PLAY</div>
+                  </div>
+                </div>
+              </div>
+              <div className="policy-text">
+                <p className="brokerage-services">
+                  Brokerage services by Cash App Investing LLC, member FINRA / SIPC. <br />
+                  See our BrokerCheck. Investing involves risk; you may lose money. Bitcoin trading offered by Cash App.
+                  Cash App Investing does not trade bitcoin and Cash App is not a member of FINRA or SIPC. Cash App
+                  facilitates banking services through Sutton Bank and Lincoln Savings Bank, Members FDIC.
+                </p>
+              </div>
+              <img
+                className="buttons-2"
+                alt="Buttons"
+                src="https://generation-sessions.s3.amazonaws.com/269a3452e1bde0a42651d0f732bc5a39/img/buttons.svg"
+              />
+              <img
+                className="vector-2"
+                alt="Vector"
+                src="https://generation-sessions.s3.amazonaws.com/269a3452e1bde0a42651d0f732bc5a39/img/vector.svg"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
